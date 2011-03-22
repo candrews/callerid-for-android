ebba342e62a266a39740261828058a8521b2d05a
Craig Andrews <candrews@integralblue.com>
2011-03-21 23:53:05 -0400

    Make the (really dumb) unit test "work" (or at least, not fail)

--- application-it/src/main/java/com/integralblue/callerid/test/MainActivityTest.java
+++ application-it/src/main/java/com/integralblue/callerid/test/MainActivityTest.java
@@ -6,7 +6,7 @@ import com.integralblue.callerid.*;
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     public MainActivityTest() {
-        super(MainActivityTest.class.getPackage().getName(),MainActivity.class);
+        super("com.integralblue.callerid",MainActivity.class);
     }
 
     public void testActivity() {
