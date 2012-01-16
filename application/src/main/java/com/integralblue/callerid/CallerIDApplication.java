9f000a50b3fddfa79c2bfc96fbfb895281a3ddc7
Craig Andrews <candrews@integralblue.com>
2012-01-15 20:57:22 -0500

    Use ICS's android.net.http.HttpResponseCache if it's available

--- application/src/main/java/com/integralblue/callerid/CallerIDApplication.java
+++ application/src/main/java/com/integralblue/callerid/CallerIDApplication.java
@@ -1,5 +1,6 @@
 package com.integralblue.callerid;
 
+import java.io.File;
 import java.lang.reflect.Method;
 
 import roboguice.util.Ln;
@@ -7,6 +8,7 @@ import roboguice.util.Ln;
 import android.app.Application;
 import android.app.Instrumentation;
 import android.content.pm.ApplicationInfo;
+import android.os.Build;
 
 public class CallerIDApplication extends Application {
 	
@@ -37,7 +39,38 @@ public class CallerIDApplication extends Application {
 	        	Ln.d(e, "Strict mode not available");
 	        }
 		}
+		
+        disableConnectionReuseIfNecessary();
+        
+        //enable the http response cache in a thread to avoid a strict mode violation
+        new Thread(){
+			@Override
+			public void run() {
+				enableHttpResponseCache();
+			}
+        }.start();
 	}
+	
+    private void enableHttpResponseCache() {
+    	// see http://android-developers.blogspot.com/2011/09/androids-http-clients.html
+        final long httpCacheSize = 10 * 1024 * 1024; // 10 MiB
+        final File httpCacheDir = new File(getCacheDir(), "http");
+        try {
+            Class.forName("android.net.http.HttpResponseCache")
+                .getMethod("install", File.class, long.class)
+                .invoke(null, httpCacheDir, httpCacheSize);
+        } catch (Exception httpResponseCacheNotAvailable) {
+            Ln.d(httpResponseCacheNotAvailable, "android.net.http.HttpResponseCache not available, probably because we're running on a pre-ICS version of Android.");
+        }
+    }
+
+    private void disableConnectionReuseIfNecessary() {
+        // HTTP connection reuse which was buggy pre-froyo
+    // see http://android-developers.blogspot.com/2011/09/androids-http-clients.html
+        if (Integer.parseInt(Build.VERSION.SDK) < Build.VERSION_CODES.FROYO) {
+            System.setProperty("http.keepAlive", "false");
+        }
+    }
 
 	public boolean isDebugMode() {
 		// check if android:debuggable is set to true
